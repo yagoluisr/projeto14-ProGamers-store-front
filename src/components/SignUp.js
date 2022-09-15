d76af1c5ad224df24f43905d312db6b118cf21e0
yagoluis <yagoluisroberto@gmail.com>
2022-09-15 16:43:11 -0300

    /sign-up: Concliída, interação com o back-end e link para a rota '/'

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,8 +1,12 @@
 import { useState } from "react"
 import styled from "styled-components"
 import { Button, Input } from "./Components"
+import { register } from "../services/progamers";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
+    const navigate = useNavigate();
+
     const [data, setData] = useState({
         name: '',
         email: '',
@@ -16,8 +20,6 @@ export default function SignUp() {
         });
     }
 
-    console.log(data)
-
     function handleRegister (e) {
         e.preventDefault();
 
@@ -25,8 +27,11 @@ export default function SignUp() {
 
         delete data.ConfirmPassword
         
-        //promise para o back-end
-
+        register(data).then(() => {
+            navigate('/');
+        }).catch(res => {
+            alert(res.response.data)
+        });
     }
 
     return (
@@ -41,7 +46,7 @@ export default function SignUp() {
                     type = 'text'
                     value = {data.name}
                     onChange = {updateData}
-                    //required
+                    required
                 ></Input>
     
                 <Input
@@ -50,7 +55,7 @@ export default function SignUp() {
                     type = 'email'
                     value = {data.email}
                     onChange = {updateData}
-                    //required
+                    required
                 ></Input>
 
                 <Input
@@ -59,7 +64,7 @@ export default function SignUp() {
                     type = 'password'
                     value = {data.password}
                     onChange = {updateData}
-                    //required
+                    required
                 ></Input>
 
                 <Input
@@ -68,11 +73,16 @@ export default function SignUp() {
                     type = 'password'
                     value = {data.ConfirmPassword}
                     onChange = {updateData}
-                    //required
+                    required
                 ></Input>
                 
                 <Button type='submit'>Cadastrar</Button>
             </Form>
+
+            <Link to = '/'>
+                <HasLogin>Já tem cadastro? Entre agora! </HasLogin>
+            </Link>
+
         </Container>
     )
 }
@@ -87,21 +97,30 @@ const Form = styled.form`
 
 `
 
+const HasLogin = styled.span`
+    font-weight: 400;
+    font-size: 18px;
+    color: #FFFFFF;
+`
+
 const Container = styled.div`
     max-width: 500px;
+    width: 90%;
 
     display: flex;
     flex-direction: column;
     align-items: center;
     margin: 0 auto 0 auto;
 
-    width: 90%;
+    a {
+        margin-top: 25px;
+    }
 `
 
 const LogoName = styled.span`
     color: #ffffff;
 
-    //font-family: 'Urbanist', sans-serif;
+    font-family: 'Urbanist', sans-serif;
     font-weight: 700;
     font-size: 44px;
 `
