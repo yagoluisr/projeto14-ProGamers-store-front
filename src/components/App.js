78884d0ec47c29a9ef5f619960e38783bbae8048
yagoluis <yagoluisroberto@gmail.com>
2022-09-16 14:10:11 -0300

    Layout implementado, link para cadastro, redirecionamento pra home, alert com erro

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,23 @@ import GlobalStyle from "../assets/styles/globalStyles";
 import Teste from "./Teste";
 import UserContext from "../contexts/User.context";
 import SignUp from "./SignUp";
+import SignIn from "./SignIn";
+import { useState } from "react";
 
 //se n aparecer nada coloca /test na url
 
 export default function App(){
+    const [token, setToken] = useState('');
 
     return(
         <BrowserRouter>
             <GlobalStyle/> 
-            <UserContext.Provider>
+            <UserContext.Provider  value={{token,setToken}}>
             <Routes>
                 <Route path='/test' element={<Teste/>}/>
                 <Route path='/sign-up' element={<SignUp/>}/>
+                <Route path='/' element={<SignIn/>}/>
+                {/* <Route path='/' element={<SignIn/>}> */}
             </Routes>   
             </UserContext.Provider>
         </BrowserRouter>
---
+++ src/components/SignIn.js
@@ -0,0 +1,66 @@
+import { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import UserContext from "../contexts/User.context";
+import { login } from "../services/progamers";
+import { Input } from "./Components";
+import { Container, Form, HasLogin, LogoName, NewButton } from "./SignUp";
+
+export default function SignIn() {
+    const { token, setToken } = useContext(UserContext);
+
+    const navigate = useNavigate();
+
+    const [data, setData] = useState({
+        email:'',
+        password:''
+    });
+
+    function updateData(e) {
+        setData({
+            ...data,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    function handleRegister (e) {
+        e.preventDefault();
+
+        login(data).then(() => {
+            navigate('/home');
+        }).catch(res => {
+            alert(res.response.data)
+        });
+    }
+
+    return (
+        <Container>
+                <LogoName>ProGamers</LogoName>
+
+                <Form onSubmit={handleRegister} >
+                    <Input
+                        placeholder='E-mail'
+                        name = 'email'
+                        type = 'email'
+                        value = {data.email}
+                        onChange = {updateData}
+                        required
+                    ></Input>
+
+                    <Input
+                        placeholder='Senha'
+                        name = 'password'
+                        type = 'password'
+                        value = {data.password}
+                        onChange = {updateData}
+                        required
+                    ></Input>
+                
+                <NewButton type='submit'>Login</NewButton>
+
+                <Link to = '/'>
+                    <HasLogin> Primeira vez? Cadastre-se! </HasLogin>
+                </Link>
+            </Form>
+        </Container>
+    )
+}
